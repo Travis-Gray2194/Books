c68f3295c4eee33e4078d4d6aa008ff69e77bc0b
Travis-Gray2194 <travisogray1@gmail.com>
2017-10-16 17:03:02 -0400

    2nd Commit changed for New ArrayList example using a new class

--- src/Books.java
+++ src/Books.java
@@ -1,52 +1,54 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Books {
     public static void main(String[] args) {
+        ArrayList<Book> library = new ArrayList<Book>();
 
         String yes="yes";
         Scanner keyboard = new Scanner(System.in);
-        int counter = 0;
-        int size = 25;
-
-
-        //Allocates memory for array
-       String title[] = new String[size];
-        String author[] = new String[size];
-       String description[] = new String[size];
-
-
   do {
 
 
-//Store user input into array
+      //Add new book
+      Book book1 = new Book();
+      book1.author = "Author Book 1";
+      book1.title = "Title of Book 1";
+      book1.description = "Description of Book 1";
+      library.add(book1);
+
+//Store user input into object from Book class array
         System.out.println("Please enter Book Title");
-        title[counter] = keyboard.nextLine();
+        book1.title = keyboard.nextLine();
 
         System.out.println("Please enter Book Author");
-        author[counter] = keyboard.nextLine();
+        book1.author = keyboard.nextLine();
 
 
         System.out.println("Please enter Book Description");
-        description[counter] = keyboard.nextLine();
+        book1.description = keyboard.nextLine();
+
 
 
       System.out.println("Would you like to enter in more books Yes/No");
       yes = keyboard.nextLine();
-        counter++;//Runing counter to keep track of stored variables in array
 
 
 //if user enters yes run loop and if user enters no run for loop
   }while (yes.equalsIgnoreCase("Yes")) ;
 //When user is done typing books print all book in array them on next line
 
-        //using i as counter to check books from array
-        for (int i = 0; i < counter; i++)
+        //for each loop getting books from array library collection input from user (taking the first element)
+        for (Book book1:library)
         {
-            System.out.print(title[i]);
+            System.out.println("All Books Listed: ");
+            System.out.println("------------------------------");
+            System.out.print(book1.title);
             System.out.println(" ");
-            System.out.print(author[i]);
+            System.out.print(book1.author);
             System.out.println(" ");
-            System.out.print(description[i]);
+            System.out.print(book1.description);
 
 
         }
